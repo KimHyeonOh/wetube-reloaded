771fe20c3bb849d9fb217703bc35c60174d8a71b
kho <kho0432@nate.com>
2023-10-30 07:28:50 +0900

    for Challenge Finish

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
+import session from "express-session";
+import flash from "express-flash";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
-
-const PORT = '4000';
-
-console.log(process.cwd());
+import userRouter from "./routers/userRouter";
+import apiRouter from "./routers/apiRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -14,10 +15,23 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use("/", globalRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+app.use(flash());
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
-const handleListening = () => console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@@@ 현오의 Server Start : http://localhost:${PORT} @@@\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@`);
-
-app.listen(PORT, handleListening);
+export default app;
